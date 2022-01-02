541ef75fb1b8e3e56aa1c3196dd84e5099a3946a
Tenzin <tenthinten@gmail.com>
2022-01-02 23:43:24 +0530

    able to fix one content post info

--- src/components/Images.js
+++ src/components/Images.js
@@ -1,12 +1,12 @@
 const image = () => {
     return (
         <div class="row">
-            <div class="column">
+            {/* <div class="column">
                 <a href="https://mostnovel.com/manga/this-curse-is-just-awesome/">
                 <img src="https://mostnovel.com/wp-content/uploads/2021/04/This-Curse-Is-Just-Awesome-1-193x278.jpg" alt="Snow" class="img-thumbnail" />
                 </a>
-            </div>
-            <div class="column">
+            </div> */}
+            {/* <div class="column">
                 <a href="https://mostnovel.com/manga/i-can-track-everything-novel/">
                 <img src="https://mostnovel.com/wp-content/uploads/2021/01/i-can-track-everything-mostnovel.jpg" alt="Snow"  class="img-thumbnail" />
                 </a>
@@ -15,7 +15,7 @@ const image = () => {
                 <a href="https://mostnovel.com/manga/sealed-divine-throne/">
                 <img src="https://mostnovel.com/wp-content/uploads/2020/07/Sealed-Divine-Throne-193x278-1.jpg" alt="Snow"  class="img-thumbnail" />
                 </a>
-            </div>
+            </div> */}
         </div>
     )
 }
--- src/components/Post.js
+++ src/components/Post.js
@@ -3,20 +3,17 @@ import "./Post.css"
 export default function post() {
     return (
         <div className="post">
-            <div className="postItem">
-                <span className="postTitle">This Curse is Awesome</span>
-                <img src="https://mostnovel.com/wp-content/uploads/2021/04/This-Curse-Is-Just-Awesome-1-193x278.jpg"
-                alt=""
-                />
-                <p>JavaScript is the world most popular 
-                lightweight, interpreted compiled programming 
-                language. It is also known as scripting 
-                language for web pages. It is well-known for 
-                the development of web pages, many non-browser 
-                environments also use it. JavaScript can be 
-                used for Client-side developments as well as 
-                Server-side developments.
-                </p>
+            <img className="postImg" 
+            src="https://mostnovel.com/wp-content/uploads/2021/04/This-Curse-Is-Just-Awesome-1-193x278.jpg"
+            alt=""
+            />
+            <div className="postInfo">
+                <span className="postTitle">This Curse is Awesome</span>               
+                    <p>JavaScript is the world most popular 
+                    lightweight, interpreted compiled programming 
+                    language. It is also known as scripting 
+                    language for web pages.
+                    </p>
             </div>
             
         </div>
