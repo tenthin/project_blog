93e8aca311da165db2c5a94abb57a19e2b3a6968
Tenzin <tenthinten@gmail.com>
2022-01-04 19:18:20 +0530

    routing multiple files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,49 @@
 import Topbar from "./components/topbar/Topbar";
 import Register from "./components/page/register/Register"
 import Login from "./components/page/login/Login"
+import Home from "./components/page/home/Home";  
 import Settings from "./components/page/settings/Settings"
 import Write from "./components/page/write/Write"
 import Single from "./components/page/single/Single";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 
 function App () {
   return (
-    <>
+    <Router>
       <Topbar/>
-      {/* <Home /> */}
-      {/* <Single/> */}
-      {/* <Write/> */}
-      {/* <Settings /> */}
-      {/* <Login/> */}
-      <Register/>
-    </>
+      <Switch>
+        <Route exact path="/">
+            <Home />
+        </Route>
+
+        <Route path="/register">
+            <Register />
+        </Route>
+
+        <Route path="/login">
+            <Login />
+        </Route>
+
+        <Route path="/write">
+            <Write />
+        </Route>
+
+        <Route path="/settings">
+            <Settings />
+        </Route>
+
+        <Route path="/post/:postID">
+            <Single />
+        </Route>
+
+      </Switch>
+    </Router>
   )
 }
 
@@ -23,4 +51,4 @@ export default App
 
 
 
-// import Home from "./components/page/home/Home";  
+      {/* <Single/> */}
